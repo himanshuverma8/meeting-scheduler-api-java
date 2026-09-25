244516a452d127531f8cf6cea8a98baa06c2dc65
Himanshu <hvinprimary@gmail.com>
2026-09-25 22:52:21 +0530

    read JWT settings from environment variables and document required vars

---
+++ src/main/java/com/hv/meeting_scheduler_api_java/controller/EventTypeController.java
@@ -0,0 +1,4 @@
+package com.hv.meeting_scheduler_api_java.controller;
+
+public class EventTypeController {
+}
---
+++ src/main/java/com/hv/meeting_scheduler_api_java/service/EventTypeService.java
@@ -0,0 +1,4 @@
+package com.hv.meeting_scheduler_api_java.service;
+
+public class EventTypeService {
+}
